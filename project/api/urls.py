829b1459bbc01484accd0afafd98e4e5234400a2
Yann Howe <yannhowe@gmail.com>
2018-07-31 04:45:31 +0000

    added missionaries and updates to api

--- project/api/urls.py
+++ project/api/urls.py
@@ -5,6 +5,8 @@ from project.api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'missionaries', views.MissionaryViewSet)
+router.register(r'updates', views.UpdateViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- project/api/views.py
+++ project/api/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.contrib.auth.models import User
+from .models import Missionary, Update
+
 from rest_framework import viewsets
-from project.api.serializers import UserSerializer
+from project.api.serializers import UserSerializer, MissionarySerializer, UpdateSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -12,3 +14,17 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class MissionaryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Missionary.objects.all()
+    serializer_class = MissionarySerializer
+
+class UpdateViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Update.objects.all()
+    serializer_class = UpdateSerializer
