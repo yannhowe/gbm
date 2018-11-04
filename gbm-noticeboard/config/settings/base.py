d403c4120f960000d589642d9ac25d0cb83af042
yannhowe <yannhowe@gmail.com>
2018-11-04 16:02:28 +0800

    fixed postgress password env varilable name

--- gbm-noticeboard/config/settings/base.py
+++ gbm-noticeboard/config/settings/base.py
@@ -114,7 +114,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME', ''),
         'USER': os.getenv('DB_USER', ''),
-        'PASSWORD': os.getenv('DB_PASS', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', '')
     }
